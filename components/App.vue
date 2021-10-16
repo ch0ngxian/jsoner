410d3d05254c25398c2bda80f8a14ae70d5b9c16
Chong Xian <chongxian@easystore.co>
2021-10-16 19:11:41 +0800

    Added resize function

--- components/App.vue
+++ components/App.vue
@@ -2,21 +2,29 @@
   <div
     class="h-screen w-screen flex justify-center text-sm"
     style="color: #d4d4d4"
+    @mouseup="endDragging"
   >
     <textarea
-      class="
-        w-full
-        h-screen
-        p-7
-        resize-none
-        focus:outline-none
-        overflow-y-scroll
-      "
-      style="background-color: #1e1e1e; min-width: 40vw; max-width: 60vw"
+      class="h-screen p-7 resize-none focus:outline-none overflow-y-scroll"
+      :style="{
+        'background-color': '#1e1e1e',
+        width: `${dividerPosition}%`,
+      }"
       v-model="input"
     ></textarea>
-    <div class="border h-screen" style="border-color: #444444"></div>
-    <div class="w-full h-screen p-7 overflow-y-scroll flex-grow break-words">
+    <div
+      class="divider"
+      :style="{
+        left: `${dividerPosition}%`,
+      }"
+      @mousedown="startDragging"
+    ></div>
+    <div
+      class="w-full h-screen p-7 overflow-y-scroll flex-grow break-words"
+      :style="{
+        width: `${100 - dividerPosition}%`,
+      }"
+    >
       <v-node v-if="isValidJson" :node="node" :showEndComma="false"></v-node>
       <p v-else>
         {{ input }}
@@ -42,6 +50,7 @@ export default defineComponent({
       input:
         '{"str":"a", "obj":{"a": "1"}, "arr":[1,2,4], "bool": true, "empty": null}',
       node: {},
+      dividerPosition: 50,
     };
   },
   computed: {
@@ -61,8 +70,32 @@ export default defineComponent({
       } catch {}
     },
   },
+  methods: {
+    handleDragging(event: Event) {
+      const clientX = (<DragEvent>event).clientX;
+      const percentage = (clientX / window.outerWidth) * 100;
+
+      if (percentage >= 30 && percentage <= 70) {
+        this.dividerPosition = percentage;
+      }
+    },
+    startDragging() {
+      document.addEventListener("mousemove", this.handleDragging);
+    },
+    endDragging() {
+      document.removeEventListener("mousemove", this.handleDragging);
+    },
+  },
   created() {
     this.node = JSON.parse(this.input);
   },
 });
 </script>
+
+<style>
+.divider {
+  background-color: #444444;
+  cursor: ew-resize;
+  @apply w-0.5 h-screen absolute hover:bg-blue-600;
+}
+</style>
